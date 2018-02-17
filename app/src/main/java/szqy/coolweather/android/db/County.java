8acb3fc655c692cb7eab4fa0bc43750abc4f3d1a
Chronaxia <972625961@qq.com>
2018-02-17 23:32:36 +0800

    完成遍历省市县

--- app/src/main/java/szqy/coolweather/android/db/County.java
+++ app/src/main/java/szqy/coolweather/android/db/County.java
@@ -8,15 +8,15 @@ import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport{
     private int id;
-    private String countryName;
+    private String countyName;
     private String weatherId;
     private int cityId;
 
     public County() {}
 
-    public County(int id, String countryName, String weatherId, int cityId) {
+    public County(int id, String countyName, String weatherId, int cityId) {
         this.id = id;
-        this.countryName = countryName;
+        this.countyName = countyName;
         this.weatherId = weatherId;
         this.cityId = cityId;
     }
@@ -29,12 +29,12 @@ public class County extends DataSupport{
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
